4b5a63aacd6de8b009697969ef22b9acc3e1f3bf
Nicolas Nahuelpan <nicolas.nahuelpan@greatplacetowork.com>
2019-04-25 14:31:09 -0400

    notice's messages updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,11 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $('#notice_wrapper').fadeOut("slow", function () {
+            $(this).remove();
+        })
+    }, 1300);
+});
